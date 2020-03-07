7429f6c6ed9b80d8b49a5c8c6780b456e98cc5ec
cmamon <christophe.quenette@gmail.com>
2020-03-07 15:46:25 +0100

    Server project linting clean

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const compression = require('compression');
-const csrf = require('csurf');
 const express = require('express');
 const helmet = require('helmet');
-const mongoose = require('mongoose');
 const socketio = require('socket.io');
-
-require('./models/user_model');
-const routes = require('./config/routes');
 
 const app = express();
 
@@ -17,21 +12,20 @@ app.use(cors());
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use(csrf({ cookie: true }));
 
 app.use(express.static('public'));
 
-let port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 const server = app.listen(port, () => {
-  console.log('\x1b[32m%s\x1b[0m', 'Server running on port ' + port);
+  console.log('\x1b[32m%s\x1b[0m', `Server running on port ${port}`);
 });
 
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-  let clientIp = socket.request.connection.remoteAddress;
-  console.log('New connection from ' + clientIp);
+  const clientIp = socket.request.connection.remoteAddress;
+  console.log(`New connection from ${clientIp}`);
 
   socket.on('message', (msg) => {
     io.emit('message', msg);
