b95d81491f1e5434f5f5f5fbb98134a36589d114
cmamon <christophe.quenette@gmail.com>
2020-11-27 02:20:49 +0100

    Implement base chat exemple from socket.io with update to version 3

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const compression = require('compression');
 const express = require('express');
 const helmet = require('helmet');
 const socketio = require('socket.io');
 
 const app = express();
 
-app.use(compression());
 app.use(cors());
 app.use(helmet());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 app.use(express.static('public'));
 
 const port = process.env.PORT || 8080;
 
 const server = app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log('\x1b[32m%s\x1b[0m', `Server running on port ${port}`);
 });
 
@@ -25,9 +23,15 @@ const io = socketio(server);
 
 io.on('connection', (socket) => {
   const clientIp = socket.request.connection.remoteAddress;
+  // eslint-disable-next-line no-console
   console.log(`New connection from ${clientIp}`);
 
   socket.on('message', (msg) => {
     io.emit('message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    // eslint-disable-next-line no-console
+    console.log(`User ${clientIp} disconnected`);
   });
 });
