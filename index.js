3aa6f09d71be58aaf09ab8ed165dc7dea0561531
cmamon <christophe.quenette@gmail.com>
2020-12-06 09:10:16 +0100

    Change static serve of client files to providing connection for a remote front-end app

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
 
 const port = process.env.PORT || 8080;
 
@@ -19,19 +18,24 @@ const server = app.listen(port, () => {
   console.log('\x1b[32m%s\x1b[0m', `Server running on port ${port}`);
 });
 
-const io = socketio(server);
+const io = socketio(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+});
 
 io.on('connection', (socket) => {
   const clientIp = socket.request.connection.remoteAddress;
-  // eslint-disable-next-line no-console
-  console.log(`New connection from ${clientIp}`);
+
+  io.emit('join', `${clientIp} has joined the chat`);
 
   socket.on('message', (msg) => {
     io.emit('message', msg);
   });
 
   socket.on('disconnect', () => {
-    // eslint-disable-next-line no-console
-    console.log(`User ${clientIp} disconnected`);
+    io.emit('left', `${clientIp} has left the chat`);
   });
 });
